e803df599e1ef196525d0cd5836896fc1bdd1fbf
Rumisaa Kamel <rnrumisaadev@gmail.com>
2026-02-11 19:14:09 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,7 +22,7 @@ export default function Footer() {
         {/* ✅ Social Icons (Center) */}
         <div className="footer-socials">
            <a href="https://wa.me/201282917996" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
-           <a href="https://www.linkedin.com/in/rumisaa-kamel-008664371?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+           <a href="https://www.linkedin.com/in/rumisaa-kamel-008664371/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
            <a href="https://www.facebook.com/share/1Ch9TM88bC/" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
            <a href="https://github.com/Rumisaa-Kamel" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
                     
